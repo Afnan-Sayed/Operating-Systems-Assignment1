0ca96cf01ce0873ee6cb8f506a176f36d30bc30d
Afnan-Sayed <afnansayed50@gmail.com>
2024-10-29 15:36:42 +0300

    Redirection classes updated

--- src/System Commands/RdirectAppend.java
+++ src/System Commands/RdirectAppend.java
@@ -7,17 +7,7 @@ public class RdirectAppend implements RedirectionInterface{
     @Override
     public void redirectOutput(String commandOutput, String fileName)
     {
-        File file = new File(fileName);
-
-        try (FileWriter writer = new FileWriter(file, false)) 
-        { 
-            writer.write(commandOutput);
-        }
         
-        catch (IOException e) 
-        {
-            System.out.println("Error writing to file: " + fileName);
-        }
     }
 }
 
