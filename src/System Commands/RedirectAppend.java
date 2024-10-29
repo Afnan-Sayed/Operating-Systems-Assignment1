700a52c1e7b98047e1b9150d83d60b89fd953d42
Afnan-Sayed <afnansayed50@gmail.com>
2024-10-29 15:59:35 +0300

    Redirection classes updated

--- src/System Commands/RedirectAppend.java
+++ src/System Commands/RedirectAppend.java
@@ -3,59 +3,56 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Scanner;
 
-public class RedirectOverwrite implements RedirectionInterface 
+public class RedirectAppend implements RedirectionInterface 
 {
     @Override
     public void redirectOutput(String commandOutput, String fileName) 
-    { 
+    {
         //case: no argument specified
-        if (fileName == null || fileName.trim().isEmpty())
+        if (fileName == null || fileName.trim().isEmpty()) 
         {
             System.out.println("Syntax Error: File name is required");
             return;
         }
-
+        
         else 
         {
-
             File file = new File(fileName);
             if (!file.isFile())
             {
                 System.out.println(fileName+ "is a directory");
                 return;
             }
-
             
             else if (commandOutput.equals("cat")) 
             {
                 System.out.println("Enter text to be written to the file: " + fileName + " (type 'stop' to finish): ");
                 
-                //the following will automatically overwrite if the file exists or creates it in case it does not exist    
-                try (Scanner scanner = new Scanner(System.in); FileWriter writer = new FileWriter(file,false)) 
+                //the following will automatically append if the file exists or creates it in case it does not exist
+                try (Scanner scanner = new Scanner(System.in);
+                     FileWriter writer = new FileWriter(file, true))
                 {
                     String line;
-                    while (!(line = scanner.nextLine()).equalsIgnoreCase("stop"))
-                    {
+                    while (!(line = scanner.nextLine()).equalsIgnoreCase("stop")) {
                         writer.write(line + System.lineSeparator());
-                        writer.flush(); 
+                        writer.flush();
                     }
-                } 
+                }
                 
                 catch (IOException e) 
                 {
                     System.out.println("Error writing to file: " + fileName + ", " + e.getMessage());
                 }
             }
-        
             
-            // dafault case: command > file
+            // default case: command >> file
             else 
-            { 
-                try (FileWriter writer = new FileWriter(file, false)) 
+            {
+                try (FileWriter writer = new FileWriter(file, true))
                 {
                     writer.write(commandOutput);
-                    System.out.println("Output written to file: " + fileName);
-                } 
+                    System.out.println("Output appended to file: " + fileName); 
+                }
                 
                 catch (IOException e) 
                 {
@@ -63,5 +60,5 @@ public class RedirectOverwrite implements RedirectionInterface
                 }
             }
         }
-     } 
+    }
 }
