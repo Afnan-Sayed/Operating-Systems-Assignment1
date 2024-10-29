d099cb918e5bad3a82b101e4d2f7a682cbc8ed85
Afnan-Sayed <afnansayed50@gmail.com>
2024-10-29 17:24:00 +0300

    Exit class is implemented

--- src/Internal Commands/ExitCommand.java
+++ src/Internal Commands/ExitCommand.java
@@ -1,9 +1,7 @@
 public class ExitCommand implements CommandInterface{
     @Override 
     public void execute(String... args) {
-    
+        System.out.println("Exiting...");
+        System.exit(0);
     }
 }
-
-
-
