7a19c8087f5232031b533ea2d077b33a9ca45350
Afnan-Sayed <afnansayed50@gmail.com>
2024-11-08 11:52:12 +0200

    implementation completed

---
+++ Test/org/example/HelpTest.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HelpTest {
+    @Test
+    void testExecute()
+    {
+        Help helpCommand = new Help();
+        String result = helpCommand.execute();
+
+        assertTrue(result.contains("Available Commands:"));
+        assertTrue(result.contains("exit                          : Terminate the program."));
+    }
+
+}
